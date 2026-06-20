68c7ccad4d3442288b088facc4f525e328a4e61f
Ghorasaini Aakash <ghorasainiaakash25@gmail.com>
2026-06-20 17:11:40 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/sorapay",
+  assetPrefix: "/sorapay",
   images: {
     unoptimized: true,
   },
